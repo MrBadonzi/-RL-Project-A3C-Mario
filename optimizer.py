71ce98e9c212eb17494aa2de548cf4c4da035d62
Andrea Marzo <83232320+MrBadonzi@users.noreply.github.com>
2022-12-29 19:38:48 +0100

    changed optimizer

--- optimizer.py
+++ optimizer.py
@@ -1,5 +1,7 @@
 from torch.optim import Adam
 from torch import zeros_like
+from torch.optim import RMSprop
+
 
 class GlobalAdam(Adam):
     def __init__(self, params, lr):
@@ -7,8 +9,27 @@ class GlobalAdam(Adam):
         for group in self.param_groups:
             for p in group['params']:
                 state = self.state[p]
+
                 state['step'] = 0
                 state['exp_avg'] = zeros_like(p.data) #exponentially decaying average of past gradients
                 state['exp_avg_sq'] = zeros_like(p.data) #exponentially decaying average of past squared gradients to provide an adaptive learning rate
                 state['exp_avg'].share_memory_() #moves the exp_avg from storage to the shared memory, thereafter to all agents in multiprocessing
                 state['exp_avg_sq'].share_memory_()
+
+
+
+class GlobalRMSprop(RMSprop):
+    def __init__(self, params, lr):
+        super(GlobalRMSprop, self).__init__(params, lr)
+
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+
+                state['step'] = 0
+                state['exp_avg'] = zeros_like(p.data) #exponentially decaying average of past gradients
+                state['square_avg'] =  zeros_like(p.data)
+                state['exp_avg_sq'] = zeros_like(p.data) #exponentially decaying average of past squared gradients to provide an adaptive learning rate
+                state['exp_avg'].share_memory_() #moves the exp_avg from storage to the shared memory, thereafter to all agents in multiprocessing
+                state['exp_avg_sq'].share_memory_()
+                state['square_avg'].share_memory_()
