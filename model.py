d5f79ce5e2a54090690966f5462dca2894f042d7
Gitrlma <121303103+Gitrlma@users.noreply.github.com>
2023-01-10 15:30:43 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -1,33 +1,32 @@
+from random import random, randrange
+import numpy as np
+import torch
 from torch import nn
-import torch.nn.functional as F
 
 
-class ActorCritic(nn.Module):
-    def __init__(self, num_inputs, num_actions):
-        super(ActorCritic, self).__init__()
-        self.conv1 = nn.Conv2d(num_inputs, 32, 3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.lstm = nn.LSTMCell(32 * 6 * 6, 512)
-        self.critic_linear = nn.Linear(512, 1)
-        self.actor_linear = nn.Linear(512, num_actions)
-        self._initialize_weights()
+class CNNDQN(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(CNNDQN, self).__init__()
+        self.conv1 = nn.Conv2d(input_shape, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.fc1 = nn.Linear(64*7*7, 512)
+        self.fc2 = nn.Linear(512, num_actions)
 
-    def _initialize_weights(self):
-        for module in self.modules():
-            if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
-                nn.init.xavier_uniform_(module.weight)
-                # nn.init.kaiming_uniform_(module.weight)
-                nn.init.constant_(module.bias, 0)
-            elif isinstance(module, nn.LSTMCell):
-                nn.init.constant_(module.bias_ih, 0)
-                nn.init.constant_(module.bias_hh, 0)
+    def forward(self, x):
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.relu(self.conv2(x))
+        x = nn.functional.relu(self.conv3(x))
+        x = x.reshape((-1, 64*7*7))
+        x = nn.functional.relu(self.fc1(x))
+        return self.fc2(x)
 
-    def forward(self, x, hx, cx):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-        hx, cx = self.lstm(x.view(x.size(0), -1), (hx, cx))
-        return self.actor_linear(hx), self.critic_linear(hx), hx, cx
+    def act(self, state, epsilon, args, num_actions):
+        if random() > epsilon:
+            if args.gpu:
+                state = state.cuda()
+            q_value = self.forward(state)
+            action = q_value.max(1)[1].item()
+        else:
+            action = randrange(num_actions)
+        return action
