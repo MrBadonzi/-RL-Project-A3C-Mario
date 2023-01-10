2e57d87a9a0833808781923abda61c3932b0cf21
Andrea Marzo <83232320+MrBadonzi@users.noreply.github.com>
2023-01-10 15:40:10 +0100

    fixed

--- train.py
+++ train.py
@@ -1,25 +1,21 @@
-import math
 from os import mkdir
 from os.path import join
-from random import random, randrange
-import numpy as np
 import torch
 from tensorboardX import SummaryWriter
-from torch import nn
-from all_in_one_env import wrap_environment,from_tuple_to_tensor
-from model import CNNDQN
+from all_in_one_env import wrap_environment
+from model import ActorCritic
 import torch.nn.functional as F
+from torch.distributions import Categorical
 from collections import deque
 from copy import copy
 from gym.wrappers import Monitor
 
 
-def process_train(id, global_model, global_optimizer, args,eps_start=1.0, eps_final=0.01, epsilon_decay = 100000):
+def process_train(id, global_model, global_optimizer, args):
     torch.manual_seed(123 + id) #each process is initialized with a seed for reproduction + its id so that they explore different things
-    loss_function = nn.MSELoss()
     writer = SummaryWriter(args.log_path)
     env=wrap_environment(args.environment,args.action_space)
-    model = CNNDQN(env.observation_space.shape[0], len(args.action_space))
+    model = ActorCritic(env.observation_space.shape[0], len(args.action_space))
     if args.transfer:
         model_name = join(args.saved_path, '%s.dat' % args.environment)
         model.load_state_dict(torch.load(model_name))
@@ -34,49 +30,56 @@ def process_train(id, global_model, global_optimizer, args,eps_start=1.0, eps_fi
     for episode in range(args.num_episodes):
         curr_episode += 1
         model.load_state_dict(global_model.state_dict()) #Synchronize thread-specific parameters to global ones
-        states = []
-        next_states=[]
+        #FIXME changed here
+        hx = torch.zeros((1, 512), dtype=torch.float)
+        cx = torch.zeros((1, 512), dtype=torch.float)
+        if args.gpu:
+            hx = hx.cuda()
+            cx = cx.cuda()
+        log_policies = []
+        values = []
         rewards = []
-        actions = []
-        dones=[]
+        entropies = []
+        # TODO include steps for second so it does not get to the end of the timer if stuck
         while True:
-            epsilon = eps_final + (eps_start - eps_final) * math.exp(-1 * ((episode + 1) / epsilon_decay))
-            if random() > epsilon:
-                if args.gpu:
-                    state = state.cuda()
-                q_value = model(state)
-                action = q_value.max(1)[1].item()
-            else:
-                action = randrange(env.action_space.n)
-
+            logits, value, hx, cx = model(state, hx, cx) #logits is result estimating Q value from Actor, value=state-value from the Critic
+            policy = F.softmax(logits, dim=1) #policy probabilities from actor
+            log_policy = F.log_softmax(logits, dim=1)
+            entropy = -(policy * log_policy).sum(1, keepdim=True) #entropy forces the actor to consider as much actions as possible while still maximizing the reward
+            multinomial = Categorical(policy) #assigns probabilities to each action (policy)
+            action = multinomial.sample().item() #action with greatest probability is most likely chosen
             next_state, reward, done, info = env.step(action)
             state = torch.from_numpy(next_state)
             if args.gpu:
                 state = state.cuda()
-            states.append(state)
-            next_states.append(next_state)
+            values.append(value)
+            log_policies.append(log_policy[0, action])
             rewards.append(reward)
-            actions.append(action)
-            dones.append(done)
+            entropies.append(entropy)
             if done:
                 state = torch.from_numpy(env.reset())
                 if args.gpu:
                     state = state.cuda()
                 break
+        R = torch.zeros((1, 1), dtype=torch.float) #reaches here only if done thereafter we are at St so R=0
+        gae = torch.zeros((1, 1), dtype=torch.float)
+        if args.gpu:
+            R = R.cuda()
+            gae = gae.cuda()
+        actor_loss = 0
+        critic_loss = 0
+        entropy_loss = 0
+        next_value = R
+        for value, log_policy, reward, entropy in list(zip(values, log_policies, rewards, entropies))[::-1]:
+            gae = gae * args.gamma * args.tau
+            gae = gae + reward + args.gamma * next_value.detach() - value.detach() #Gae= gae + R − V (si; θ′v)
+            next_value = value                                                     #where R = ri + γ*R
+            R = R * args.gamma + reward      #update of R --------------------------------^
+            actor_loss = actor_loss + log_policy * gae  #TODO Accumulate gradients wrt θ′
+            critic_loss = critic_loss + (R - value) ** 2 / 2  #TODO Accumulate gradients wrt θv′
+            entropy_loss = entropy_loss + entropy
 
-        rewards = torch.FloatTensor(rewards).reshape(-1, 1)
-        actions = torch.LongTensor(np.array(actions)).reshape(-1, 1)
-        states = from_tuple_to_tensor(states)
-        next_states = from_tuple_to_tensor(next_states)
-        dones = torch.IntTensor(dones).reshape(-1, 1)
-
-        qvals = model(states)
-        qvals = torch.gather(qvals, 1, actions)
-
-        next_qvals = model(next_states)
-        next_qvals_max = torch.max(next_qvals, dim=-1)[0].reshape(-1, 1)
-        target_qvals = rewards + (1 - dones) * args.gamma * next_qvals_max
-        total_loss = loss_function(qvals, target_qvals)
+        total_loss = -actor_loss + critic_loss - args.beta * entropy_loss
         writer.add_scalar("Train_{}/Loss".format(id), total_loss, curr_episode)
         writer.add_scalar("Train_{}/Reward".format(id), reward, curr_episode)
         global_optimizer.zero_grad()
@@ -99,7 +102,7 @@ def process_test(global_model, args):
     index = 0
     saved_models = []
     env = wrap_environment(args.environment, args.action_space)
-    model = CNNDQN(env.observation_space.shape[0], len(args.action_space))
+    model = ActorCritic(env.observation_space.shape[0], len(args.action_space))
     if args.transfer:
         model_name = join(args.saved_path, '%s.dat' % args.environment)
         model.load_state_dict(torch.load(model_name))
@@ -109,6 +112,8 @@ def process_test(global_model, args):
     episode_reward = 0.0
     curr_step = 0
     curr_episode=0
+    hx = None
+    cx = None
     actions = deque(maxlen=args.max_actions)
     action_list=[]
     while True:
@@ -116,10 +121,16 @@ def process_test(global_model, args):
         flag=False
         if done:
             model.load_state_dict(global_model.state_dict())
-        if args.gpu:
-            state = state.cuda()
-        qvals = model(state)
-        action = qvals.max(1)[1].item()
+        with torch.no_grad():
+            if done:
+                hx = torch.zeros((1, 512), dtype=torch.float)
+                cx = torch.zeros((1, 512), dtype=torch.float)
+            else:
+                hx = hx.detach()
+                cx = cx.detach()
+        logit, value, hx, cx = model(state, hx, cx)
+        policy = F.softmax(logit, dim=1)
+        action = torch.argmax(policy).item() #action with greatest probability is chosen, thereafter exploitation only
         next_state, reward, done, info = env.step(action)
         if args.render:
             env.render()
@@ -188,11 +199,16 @@ def record_best_run(model, args, episode): #record video
     #Only exploitation for the recording
     episode_reward = 0.0
     state = torch.from_numpy(env.reset())
+    hx = torch.zeros((1, 512), dtype=torch.float)
+    cx = torch.zeros((1, 512), dtype=torch.float)
     while True:
-        qvals = model(state)
-        action = qvals.max(1)[1].item()
+        logit, value, hx, cx = model(state, hx, cx)
+        policy = F.softmax(logit, dim=1)
+        action = torch.argmax(policy).item()
         next_state, reward, done, _ = env.step(action)
         episode_reward += reward
         state = torch.from_numpy(next_state)
+        hx = hx.detach()
+        cx = cx.detach()
         if done:
             return
